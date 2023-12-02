e2a62f75423dfc57ec8cf3d85621219d2346ff81
Sándor Battaglini-Fischer <sandorbf274@gmail.com>
2023-12-02 22:27:33 +0100

    plots update

--- plots_final.py
+++ plots_final.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import os
-from scipy.stats import kstest, expon, anderson, shapiro
+from scipy.stats import kstest, expon, anderson, shapiro, norm, uniform
 
 fontsize = 18
 colormap = "viridis"
@@ -17,15 +17,17 @@ plt.rcParams.update({
 sns.set(style="darkgrid", font='Palatino', font_scale=1.25)
 sns.set_palette(sns.color_palette(colormap))
 
-
 os.makedirs('final_plots', exist_ok=True)
+
+
 
 def load_data(queue_model, n=None):
     file_map = {
         "MMN": "m_m_n_simulation_results.csv",
         "MDN": "m_d_n_simulation_results.csv",
         "Hyperexponential": "hyperexponential_simulation_results.csv",
-        "ShortestJobFirst": "shortest_job_first_simulation_results.csv"
+        "ShortestJobFirst": "shortest_job_first_simulation_results.csv",
+        "MM1K": "m_m_1_k_simulation_results.csv"
     }
     filename = file_map.get(queue_model, "m_m_n_simulation_results.csv")
     df = pd.read_csv(filename)
@@ -37,8 +39,10 @@ queue_model_format = {
     "MMN": {"title": "M/M/N", "filename": "MMN"},
     "MDN": {"title": "M/D/N", "filename": "MDN"},
     "Hyperexponential": {"title": "Hyperexponential", "filename": "Hyperexponential"},
-    "ShortestJobFirst": {"title": "Shortest Job First", "filename": "ShortestJobFirst"}
+    "ShortestJobFirst": {"title": "Shortest Job First", "filename": "ShortestJobFirst"},
+    "MM1K": {"title": "M/M/1/K", "filename": "MM1K"}
 }
+
 
 def plot_mean_for_n(queue_models, ns):
     for queue_model in queue_models:
@@ -60,6 +64,7 @@ def plot_mean_for_n(queue_models, ns):
             plt.legend(loc='upper right')
             plt.savefig(f'final_plots/{queue_model_format[queue_model]["filename"]}_plot_n_{n}.png')
             plt.close()
+
 
 def plot_mean_for_rho(queue_models, ns):
     for queue_model in queue_models:
@@ -85,6 +90,7 @@ def plot_mean_for_rho(queue_models, ns):
             plt.savefig(f'final_plots/{queue_model_format[queue_model]["filename"]}_plot_rho_{rho}.png')
             plt.close()
         
+
 def compare_queue_models(queue_models, n, rho):
     plt.figure(figsize=(10, 7))
 
@@ -115,6 +121,7 @@ def compare_queue_models(queue_models, n, rho):
     plt.savefig(f'final_plots/comparison_{models_filename}_rho_{rho}_N_{n}.png')
     plt.close()
 
+
 def plot_distribution_and_analyze(queue_model, n, rho):
     df = load_data(queue_model, n)
     df_rho = df[df['rho'] == rho]
@@ -129,29 +136,53 @@ def plot_distribution_and_analyze(queue_model, n, rho):
 
     lambda_est = 1 / np.mean(data)
     
-    ks_stat, ks_p_value = kstest(data, expon(scale=lambda_est).cdf)
-    ad_stat, _, _ = anderson(data, dist='expon')
-    shapiro_stat, shapiro_p_value = shapiro(data)
+    ks_stat_exp, _ = kstest(data, expon(scale=lambda_est).cdf)
+    ks_stat_norm, _ = kstest(data, norm(loc=np.mean(data), scale=np.std(data)).cdf)
+    ks_stat_unif, _ = kstest(data, uniform(loc=np.min(data), scale=np.max(data) - np.min(data)).cdf)
 
     text_props = dict(horizontalalignment='right', verticalalignment='top', 
                       transform=plt.gca().transAxes, fontsize=14, bbox=dict(facecolor='white', alpha=0.5))
 
-    plt.text(0.95, 0.95, f'KS Test Statistic: {ks_stat:.10f}\nP-value: {ks_p_value:.10f}', **text_props)
-
-    plt.text(0.95, 0.80, f'Anderson-Darling Test Statistic: {ad_stat:.10f}', **text_props)
-
-    plt.text(0.95, 0.70, f'Shapiro-Wilk Test Statistic: {shapiro_stat:.10f}\nP-value: {shapiro_p_value:.10f}', **text_props)
+    plt.text(0.95, 0.95, f'KS Test (Exponential) Statistic: {ks_stat_exp:.10f}', **text_props)
+    plt.text(0.95, 0.85, f'KS Test (Normal) Statistic: {ks_stat_norm:.10f}', **text_props)
+    plt.text(0.95, 0.75, f'KS Test (Uniform) Statistic: {ks_stat_unif:.10f}', **text_props)
 
     plt.savefig(f'final_plots/distribution_{queue_model_format[queue_model]["filename"]}_N_{n}_rho_{rho}.png')
     plt.close()
 
 
-ns = [1, 2, 4]
+def plot_heatmap(queue_model, ns, rhos, fixed_vmax):
+    heatmap_data = pd.DataFrame(index=rhos, columns=ns)
 
-# queue_models = ["MMN", "MDN", "Hyperexponential", "ShortestJobFirst"]
+    for n in ns:
+        for rho in rhos:
+            df = load_data(queue_model, n)
+            df_rho = df[df['rho'] == rho]
+            mean_waiting_time = df_rho['mean_waiting_time'].mean()
+            heatmap_data.loc[rho, n] = mean_waiting_time
+
+    heatmap_data = heatmap_data.apply(pd.to_numeric)
+
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(heatmap_data, annot=True, cmap='viridis', fmt=".2f", vmin=0, vmax=fixed_vmax,
+                cbar_kws={'label': 'Mean Waiting Time'})
+    plt.title(f'Mean Waiting Times for {queue_model_format[queue_model]["title"]}')
+    plt.xlabel('Number of Servers (n)')
+    plt.ylabel(fr'System load ($\rho$)')
+    plt.savefig(f'final_plots/heatmap_{queue_model_format[queue_model]["filename"]}.png')
+    plt.close()
+
+
+ns = [1, 2, 4]
+queue_models = ["MM1K"]
+
 # plot_mean_for_n(queue_models, ns)
 # plot_mean_for_rho(queue_models, ns)
 
-# compare_queue_models(["MMN", "MDN"], 1, 0.9)
+# compare_queue_models(["MMN", "Hyperexponential"], 1, 0.9)
 
-plot_distribution_and_analyze("MMN", 1, 0.9)
+# plot_distribution_and_analyze("Hyperexponential", 1, 0.9)
+
+# plot_heatmap("Hyperexponential", [1, 2, 4], [0.7, 0.8, 0.9, 0.95], fixed_vmax=20)
+
+
--- simulation.py
+++ simulation.py
@@ -10,28 +10,34 @@ class ServiceRateDistribution(Enum):
     M_D_N = 2
     HYPEREXPONENTIAL = 3
     SHORTEST_JOB_FIRST = 4
-    M_G_N = 5
+    M_M_1_K = 5
 
 def interarrival(n, rho, mu):
     return np.random.exponential(1/(n*rho*mu))
 
 def service_time(mu, distribution, general_dist_params=None):
-    if distribution in [ServiceRateDistribution.M_M_N, ServiceRateDistribution.SHORTEST_JOB_FIRST]:
+    if distribution == ServiceRateDistribution.M_M_N or distribution == ServiceRateDistribution.SHORTEST_JOB_FIRST:
         return np.random.exponential(1/mu)
     elif distribution == ServiceRateDistribution.M_D_N:
         return 1/mu
     elif distribution == ServiceRateDistribution.HYPEREXPONENTIAL:
-        return np.random.exponential(1) if np.random.rand() < 0.75 else np.random.exponential(5)
-    elif distribution == ServiceRateDistribution.M_G_N:
-        return np.random.normal(*general_dist_params)
+        return np.random.exponential(1) if np.random.rand() < 0.75 else np.random.exponential(0.2)
+    elif distribution == ServiceRateDistribution.M_M_1_K:
+        return np.random.exponential(1/mu)
+    else:
+        raise ValueError(f"Unsupported distribution type: {distribution}")
 
-def customer(env, queue, mu, waiting_times, distribution):
+def customer(env, queue, mu, waiting_times, distribution, buffer=None):
+    if buffer is not None:
+        yield buffer.put(1)
     arrival_time = env.now
     with queue.request() as req:
         yield req
         wait_time = env.now - arrival_time
         yield env.timeout(service_time(mu, distribution))
         waiting_times.append(wait_time)
+    if buffer is not None:
+        yield buffer.get(1)
 
 def customer_prio(env, queue, mu, waiting_times, distribution):
     arrival_time = env.now
@@ -43,33 +49,82 @@ def customer_prio(env, queue, mu, waiting_times, distribution):
         yield env.timeout(service_duration)
         waiting_times.append(wait_time)
 
-def source(env, queue, n, rho, mu, waiting_times, distribution):
+def source(env, queue, n, rho, mu, waiting_times, distribution, buffer=None):
     while True:
         yield env.timeout(interarrival(n, rho, mu))
-        if distribution == ServiceRateDistribution.SHORTEST_JOB_FIRST:
-            env.process(customer_prio(env, queue, mu, waiting_times, distribution))
-        else:
-            env.process(customer(env, queue, mu, waiting_times, distribution))
+        if buffer is None or buffer.level < buffer.capacity:
+            env.process(customer(env, queue, mu, waiting_times, distribution, buffer))
 
-def run_simulation(n, rho, mu, distribution, general_dist_parameters, run_time):
+def run_simulation(n, rho, mu, distribution, run_time, K=None):
     env = simpy.Environment()
-    if distribution == ServiceRateDistribution.SHORTEST_JOB_FIRST:
+    waiting_times = []
+
+    if distribution == ServiceRateDistribution.M_M_1_K and K is not None:
+        queue = simpy.Resource(env, capacity=1)  # One service channel
+        buffer = simpy.Container(env, capacity=K, init=0)  # Finite capacity buffer
+        env.process(source(env, queue, n, rho, mu, waiting_times, distribution, buffer))
+    elif distribution == ServiceRateDistribution.SHORTEST_JOB_FIRST:
         queue = simpy.PriorityResource(env, capacity=n)
+        env.process(source(env, queue, n, rho, mu, waiting_times, distribution))
     else:
         queue = simpy.Resource(env, capacity=n)
-    
-    waiting_times = []
-    env.process(source(env, queue, n, rho, mu, waiting_times, distribution))
+        env.process(source(env, queue, n, rho, mu, waiting_times, distribution))
+
     env.run(until=run_time)
     return waiting_times
 
-def analyze(ns, rhos, mu, distribution, general_dist_params, output_file):
+
+
+def analytical_mean_waiting_time_mmn(n, rho, mu):
+    if rho >= 1:
+        return float('inf')  # System is unstable for rho >= 1
+    if n == 1:
+        # M/M/1 Queue
+        return rho / (mu * (1 - rho))
+    else:
+        # M/M/n Queue using Erlang C formula
+        rho_total = n * rho
+        erlang_c_numerator = (rho_total ** n / np.math.factorial(n)) * (n / (n - rho_total))
+        erlang_c_denominator = sum([rho_total ** k / np.math.factorial(k) for k in range(n)]) + erlang_c_numerator
+        erlang_c = erlang_c_numerator / erlang_c_denominator
+        return (erlang_c / (n * mu - n * rho * mu))
+
+def analytical_mean_waiting_time_mm1k(rho, mu, K):
+    # Calculate the probability P0 that the system is empty
+    P0 = (1 - rho) / (1 - rho**(K+1))
+
+    # Calculate the mean number of customers in the system, L
+    if rho != 1:
+        L = (rho * (1 - (K+1) * rho**K + K * rho**(K+1))) / ((1 - rho) * (1 - rho**(K+1)))
+    else: # Handle the special case when rho is 1
+        L = K / 2
+
+    # Calculate the mean waiting time in the system, W
+    # Arrival rate, lambda = mu * rho
+    W = L / (mu * rho)
+
+    return W
+    
+
+    
+
+def analyze(ns, rhos, mu, distribution, output_file, K=None):
     results = []
+    t_test_results = []
 
     for n in ns:
         for rho in rhos:
+            all_waiting_times = []
+
             for _ in range(30):
-                waiting_times = run_simulation(n, rho, mu, distribution, general_dist_params, 60000)
+                if distribution == ServiceRateDistribution.M_M_1_K:
+                    # For M/M/1/K queue, pass K to the simulation
+                    waiting_times = run_simulation(n, rho, mu, distribution, 60000, K)
+                else:
+                    # For other queue types
+                    waiting_times = run_simulation(n, rho, mu, distribution, 60000)
+
+                all_waiting_times.extend(waiting_times)
                 mean_list = np.concatenate((np.arange(100, 1000, 500), np.arange(1000, 20000, 3000), np.arange(10000, 40000, 2000)))
 
                 for num_customers in mean_list:
@@ -89,11 +144,27 @@ def analyze(ns, rhos, mu, distribution, general_dist_params, output_file):
                         'number_of_customers': num_customers
                     })
 
-            print(f"Completed analysis for n={n}, rho={rho}, distribution={distribution.name}")
+            if distribution == ServiceRateDistribution.M_M_1_K:
+                analytical_mean = analytical_mean_waiting_time_mm1k(rho, mu, K)
+            else:
+                analytical_mean = analytical_mean_waiting_time_mmn(n, rho, mu)
 
-    df = pd.DataFrame(results)
-    df.to_csv(output_file)
+            t_stat, p_value = stats.ttest_1samp(all_waiting_times, analytical_mean)
+            t_test_results.append({
+                'n': n,
+                'rho': rho,
+                't_stat': t_stat,
+                'p_value': p_value
+            })
+            print(f"Completed T-test for n={n}, rho={rho}: t_stat={t_stat}, p_value={p_value}")
+
+    df_results = pd.DataFrame(results)
+    df_t_test = pd.DataFrame(t_test_results)
+    combined_df = pd.merge(df_results, df_t_test, on=['n', 'rho'], how='outer')
+    combined_df.to_csv(output_file)
     print(f"Results written to {output_file}")
+
+
 
 # Simulation parameters
 mu = 1
@@ -101,12 +172,8 @@ ns = [1, 2, 4]
 rhos = [0.7, 0.8, 0.9, 0.95]
 
 
-mean = 1
-std_dev = 0.5
-general_dist_params = (mean, std_dev)
 
-
-# Run simulations for each distribution
-for distribution in ServiceRateDistribution:
-    output_file = f"{distribution.name.lower()}_simulation_results.csv"
-    analyze(ns, rhos, mu, distribution, general_dist_params, output_file)
+distribution = ServiceRateDistribution.M_M_N
+K=10
+output_file = f"{distribution.name.lower()}_simulation_results.csv"
+analyze(ns, rhos, mu, distribution, output_file, K)
